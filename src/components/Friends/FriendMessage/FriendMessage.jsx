38936421954ccd123c3b47961d633783cb9d2b97
polakiv <polakiv@mail.ru>
2019-11-26 10:29:58 +0300

    теперь ясно что читсый ))

--- src/components/Friends/FriendMessage/FriendMessage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './../Friends.module.css';
-
-const FriendMessage = (props) => {
-    return <div className={s.friend}>{props.friendmessage}</div>
-}
-
-export default FriendMessage;
